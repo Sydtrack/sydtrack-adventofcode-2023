cd656741f8afd746a6b490b324afc4948bb80af2
Alberto Wagner <alberto.wagner@gmail.com>
2023-12-07 11:28:36 -0300

    Day 4

---
+++ src/day4/index.ts
@@ -0,0 +1,69 @@
+import { Day } from "../day";
+
+class Day4 extends Day {
+
+    constructor(){
+        super(4);
+    }
+
+    solveForPartOne(input: string): string {
+        const lines = input.split('\n');
+        let sum = 0;
+
+        for (const line of lines) {
+            if (!line) continue
+            const [card, numbers] = line.split(/:[ ]+/g)
+            const [left, right] = numbers.split(/[ ]+\|[ ]+/);
+
+            const winningNumbers = left.split(/[ ]+/).map((n) => parseInt(n))
+            const myNumbers = right.split(/[ ]+/).map((n) => parseInt(n))
+
+            let count = 0
+            for (const mine of myNumbers) {
+                if (winningNumbers.includes(mine)) {
+                    count++
+                }
+            }
+
+            if (count) {
+                sum += Math.pow(2, count - 1)
+            }
+        }
+
+        return String(sum);
+    }
+
+    solveForPartTwo(input: string): string {
+        const lines = input.split('\n');
+        const won = new Array(lines.length - 1).fill(1)
+
+        for (const [ix, line] of lines.entries()) {
+            if (!line) continue
+            const [card, numbers] = line.split(/:[ ]+/g)
+            const [left, right] = numbers.split(/[ ]+\|[ ]+/);
+
+            const winningNumbers = left.split(/[ ]+/).map((n) => parseInt(n))
+            const myNumbers = right.split(/[ ]+/).map((n) => parseInt(n))
+
+            let count = 0
+            for (const mine of myNumbers) {
+                if (winningNumbers.includes(mine)) {
+                    count++
+                }
+            }
+
+            while(count) {
+                won[ix + count] += won[ix]
+                count--
+            }
+        }
+
+        let sum = 0;
+        won.forEach((n) => sum += parseInt(n))
+
+        console.log(won)
+        return String(sum);
+    }
+}
+
+export default new Day4;
